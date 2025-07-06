b33d5e30e91b9a86163a395ba82991d032d0e44d
Samuel Quintero <quinterosanchezsamuel1@gmail.com>
2025-07-05 23:44:39 -0500

    Add files via upload

---
+++ js/middleware.js
@@ -0,0 +1,11 @@
+// middleware
+
+(function () {
+  const usuario = JSON.parse(sessionStorage.getItem("usuarioActivo"));
+
+  if (!usuario) {
+    // Si no hay sesión activa, redirige al login
+    alert("Acceso denegado. Inicia sesión primero.");
+    location.href = "../login.html";
+  }
+})();
